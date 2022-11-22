c3692287c6d3201ce1f3639d7037c8bf76ecf2dd
gavin mac pro <gavin19950511@gmail.com>
2022-11-22 23:10:40 +0800

    first commit

---
+++ test-spider/main.py
@@ -0,0 +1,53 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from crawlab import save_item
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.set_capability("browserVersion", "67")
+# chrome_options.set_capability("platformName", "Windows XP")
+
+# create web driver with chrome
+# chrome_options = Options()
+chrome_options.set_capability("browserVersion", "67")
+chrome_options.set_capability("platformName", "Windows XP")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+# browser = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Remote(
+    command_executor='http://selenium:4444',
+    options=chrome_options
+)
+
+
+# navigate to news list page
+browser.get('https://36kr.com/information/web_news/')
+
+# get article items
+items = browser.find_elements(by=By.CSS_SELECTOR, value='.information-flow-list > .information-flow-item')
+
+# iterate items
+for item in items:
+    # fields
+    el_title = item.find_element(by=By.CSS_SELECTOR, value='.article-item-title')
+    title = el_title.text
+    url = el_title.get_attribute('href')
+    topic = item.find_element(by=By.CSS_SELECTOR, value='.kr-flow-bar-motif > a').text
+    description = item.find_element(by=By.CSS_SELECTOR, value='.article-item-description').text
+    try:
+        pic_url = item.find_element(by=By.CSS_SELECTOR, value='.article-item-pic > img').get_attribute('src')
+    except:
+        pic_url = None
+
+    # save to crawlab
+    save_item({
+        'title': title,
+        'url': url,
+        'topic': topic,
+        'description': description,
+        'pic_url': pic_url,
+    })
+    print(title,url)
+print(1234)
