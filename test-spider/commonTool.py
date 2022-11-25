5aa97ed3f2e7c9d3782f6fe9351ed0c4fcc7d0c0
Wang <gavin19950511@gmail.com>
2022-11-25 08:14:12 +0000

    upgrade(spider): Update Lib for spider test

--- test-spider/commonTool.py
+++ test-spider/commonTool.py
@@ -1,11 +1,19 @@
+# commonTool
+import json
 from typing import List, Any
 
+
 def to1D(arr_2d: List[List[Any]]) -> List[Any]:
-    re_array: List[Any] = []
-    for a in arr_2d:
-        re_array.extend(a)
-    return re_array
+  re_array: List[Any] = []
+  for a in arr_2d:
+    re_array.extend(a)
+  return re_array
+
+
+def loadJsonFile(path: str):
+  with open(path, 'r') as f:
+    return json.load(f)
 
 
 ##
-assert [1, 2, 3, 4, 5] == to1D([[1],[2],[3,4],[5]])
+assert [1, 2, 3, 4, 5] == to1D([[1], [2], [3, 4], [5]])
