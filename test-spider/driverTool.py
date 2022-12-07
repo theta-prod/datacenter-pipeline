44aaee222fb43e5372cad34770101bef69f883e6
Wang <gavin19950511@gmail.com>
2022-12-07 05:36:33 +0000

    fix(spider): Add missing files

--- test-spider/driverTool.py
+++ test-spider/driverTool.py
@@ -1,5 +1,5 @@
 # DriverTool
-from typing import TypedDict, Dict, Any
+from typing import TypedDict, Dict, Any, Tuple
 from selenium import webdriver
 from actionDefination import ActionFuncDefinitionLabel, ActionLocateFunc, ActionExecuteFunc, defaultActionLocateFuncMap, defaultActionExecuteFuncMap
 
@@ -23,18 +23,15 @@ class ActionStorge(TypedDict):
 
 def initRemoteDriver(c: RemoteDriverConfig) -> webdriver.Remote:
   chrome_options = webdriver.ChromeOptions()
-  chrome_options.set_capability("browserVersion", c["chromeVersion"])  #type: ignore
-  chrome_options.set_capability("platformName", c["platformType"])  #type: ignore
+  chrome_options.set_capability("browserVersion",
+                                c["chromeVersion"])  #type: ignore
+  chrome_options.set_capability("platformName",
+                                c["platformType"])  #type: ignore
   chrome_options.add_argument("ignore-certificate-errors")  #type: ignore
-  chrome_options.add_argument('--disable-notifications')  #type: ignore
-  chrome_options.add_argument('--disable-gpu')  #type: ignore
-  chrome_options.add_argument('--headless')  #type: ignore
 
-
-  # chrome_options.add_experimental_option("prefs",{"profile.default_content_setting_values.notifications" : 2})  #type: ignore
-  driver = webdriver.Remote(command_executor=c["hostUrl"], options=chrome_options)
-  driver.implicitly_wait(5)  # seconds
-  driver.set_page_load_timeout(25) # seconds
+  driver = webdriver.Remote(command_executor=c["hostUrl"],
+                            options=chrome_options)
+  driver.implicitly_wait(25)  # seconds
   return driver
 
 
